d47a1f90d4bc9273a6ee8a24c29c98ada370cd35
Potato <potatoworkph@gmail.com>
2020-05-24 19:50:37 +0800

    change

--- chain/web/models.py
+++ chain/web/models.py
@@ -5,12 +5,17 @@ from tools.base import BaseModel
 
 
 class AppVersion(BaseModel):
-    version = models.TextField(verbose_name='版本', default='0.0.0')
-
-    flag = models.BooleanField(default=True, verbose_name='update_flat', help_text='是否强制更新 应该是强制更新')
 
     app = models.FileField(upload_to='app/',
                            default='app-release.apk', verbose_name='apk', help_text='apk 文件', )
+    version = models.TextField(verbose_name='版本', default='1.0.0')
+
+    flag = models.BooleanField(default=True, verbose_name='是否强制更新', help_text='是否强制更新 应该是强制更新')
+
+
+
+    def __str__(self):
+        return '版本'
 
     class Meta:
         verbose_name = verbose_name_plural = '版本更新'
