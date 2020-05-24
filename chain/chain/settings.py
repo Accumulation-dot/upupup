daaca23b0d3d7244918dc7666859f819f3049df6
Potato <potatoworkph@gmail.com>
2020-05-24 18:26:14 +0800

    static test

--- chain/chain/settings.py
+++ chain/chain/settings.py
@@ -138,13 +138,13 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = '/opt/nginx/static/'
+
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
